f3b48ca65f9b5121ec31173b25c18dea0247e33a
llamicron <llamicron@gmail.com>
2019-10-22 14:18:11 -0500

    Renames 'run' to 'read_register'

--- src/RTU/omega/cn7500.rs
+++ src/RTU/omega/cn7500.rs
@@ -4,24 +4,25 @@ use tokio_serial::{Serial, SerialPortSettings};
 use tokio_modbus::prelude::*;
 
 
-struct Omega {
+pub struct CN7500 {
     addr: u8,
     tty_addr: String,
     baudrate: u32
 }
 
-impl Omega {
+impl CN7500 {
 
-    pub fn new(addr: u8, tty_addr: &str, baudrate: u32) -> Omega {
-        Omega {
+    pub fn new(addr: u8, tty_addr: &str, baudrate: u32) -> CN7500 {
+        CN7500 {
             addr,
             tty_addr: String::from(tty_addr),
             baudrate
         }
     }
 
-    pub fn run<F>(&self, register: u16, cnt: u16, handler: F)
+    pub fn read_register<F>(&self, register: u16, cnt: u16, handler: F)
     where F: FnOnce(Vec<u16>) -> Result<(), std::io::Error> {
+        // TODO: Add a timeout on wrong addr
         let mut core = Core::new().unwrap();
         let handle = core.handle();
         let slave = Slave(self.addr);
@@ -31,9 +32,10 @@ impl Omega {
         let port = Serial::from_path_with_handle(self.tty_addr.as_str(), &settings, &handle.new_tokio_handle()).unwrap();
 
         let task = rtu::connect_slave(&handle, port, slave).and_then(|ctx| {
-            println!("Reading a sensor value");
             ctx
+                // Read sensor value
                 .read_holding_registers(register, cnt)
+                // Then pass it to the handler
                 .and_then(move |rsp| { handler(rsp) })
         });
 
@@ -48,8 +50,8 @@ mod tests {
 
     #[test]
     fn test_read_pv() {
-        let o = Omega::new(0x16, "/dev/ttyAMA0", 19200);
-        o.run(0x1000, 1, |rsp| {
+        let o = CN7500::new(0x16, "/dev/ttyAMA0", 19200);
+        o.read_register(0x1000, 1, |rsp| {
             println!("{:?}", rsp);
             assert!(rsp[0] > 0);
             Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use brewdrivers::RTU::omega::cn7500;
+use brewdrivers::RTU::omega::cn7500::CN7500;
 
 fn main() {
+    let omega = CN7500::new(0xff, "/dev/ttyAMA0", 19200);
+    omega.read_register(0x1001, 1, |response| {
+        println!("{:?}", response);
 
+        Ok(())
+    });
     // brewdrivers::cli::parse_args();
 }
