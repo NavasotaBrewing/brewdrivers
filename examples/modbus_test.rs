43393f77ea440045420aa542cb73f3784947e151
llamicron <llamicron@gmail.com>
2021-02-15 14:50:59 -0600

    Adds modbus tests

--- examples/modbus_test.rs
+++ examples/modbus_test.rs
@@ -1,10 +1,25 @@
 use brewdrivers::modbus::ModbusInstrument;
-use tokio_modbus::client::Reader;
 
 #[tokio::main]
 async fn main() {
+    // Testing on my OMEGA CN7500 PID
     let mut inst = ModbusInstrument::new(0x16, "/dev/ttyUSB0", 19200).await;
-    inst.write_coil(0x0814, true).await.unwrap();
 
+    // Read SV
+    let pv1 = inst.read_holding_registers(0x1001, 1).await.unwrap();
+    println!("pv is now {:?}", pv1);
+    
+    // Set SV
+    inst.write_register(0x1001, 1300).await.unwrap();
+    
+    // Read SV again
+    let pv2 = inst.read_holding_registers(0x1001, 1).await.unwrap();
+    println!("pv is now {:?}", pv2);
+    
+    // Set PID to STOP
+    inst.write_coil(0x0814, false).await.unwrap();
+    // Read run/stop value
+    let coil = inst.read_coils(0x0814, 1).await.unwrap();
+    println!("Coil 0x0814 is {:?}", coil);
 
 }
--- src/modbus.rs
+++ src/modbus.rs
@@ -165,3 +165,57 @@ impl ModbusInstrument {
         }
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio::test;
+    use serial_test::serial;
+
+    async fn instr() -> ModbusInstrument {
+        ModbusInstrument::new(0x16, "/dev/ttyUSB0", 19200).await
+    }
+
+    #[test]
+    #[serial]
+    async fn test_read_write_coil() {
+        let mut instr = instr().await;
+        let rsp1 = instr.write_coil(0x0814, true).await;
+        assert!(rsp1.is_ok());
+        let value1 = instr.read_coils(0x0814, 1).await;
+        assert!(value1.is_ok());
+        assert!(value1.unwrap()[0]);
+
+        let rsp2 = instr.write_coil(0x0814, false).await;
+        assert!(rsp2.is_ok());
+        let value2 = instr.read_coils(0x0814, 1).await;
+        assert!(value2.is_ok());
+        assert!(!value2.unwrap()[0]);
+
+    }
+
+    #[test]
+    #[serial]
+    async fn test_read_write_register() {
+        let mut instr = instr().await;
+        // Set SV in register 0x1001 to 1400
+        let rsp = instr.write_register(0x1001, 1400).await;
+        assert!(rsp.is_ok());
+
+        // Read SV register, assert we get 1400
+        let old_sv = instr.read_holding_registers(0x1001, 1).await;
+        assert!(old_sv.is_ok());
+        assert!(old_sv.unwrap()[0] == 1400);
+
+        // Set SV in register 0x1001 to 1500
+        let rsp2 = instr.write_register(0x1001, 1500).await;
+        assert!(rsp2.is_ok());
+
+        // Read SV register again, assert we get 1500
+        let new_sv = instr.read_holding_registers(0x1001, 1).await;
+        assert!(new_sv.is_ok());
+        assert!(new_sv.unwrap()[0] == 1500);
+    }
+
+}
