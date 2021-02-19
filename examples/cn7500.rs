daa7b0fd2888b6fbd82e55c7566bb284c90aded3
llamicron <llamicron@gmail.com>
2021-02-18 20:04:01 -0600

    Changes some examples

--- examples/cn7500.rs
+++ examples/cn7500.rs
@@ -1,7 +1,12 @@
-use brewdrivers::omega::{Degree, CN7500};
+use brewdrivers::omega::CN7500;
 
 #[tokio::main]
 async fn main() {
     let mut cn = CN7500::new(0x16, "/dev/ttyUSB0", 19200).await;
-    cn.set_degrees(Degree::Fahrenheit).await.unwrap();
+    // cn.set_degrees(Degree::Fahrenheit).await.unwrap();
+    if let Ok(pv) = cn.get_pv().await {
+        println!("CN7500 PV: {}", pv);
+    } else {
+        println!("Couldn't get PV from device, check the connection details!");
+    }
 }
--- examples/modbus.rs
+++ examples/modbus.rs
@@ -8,14 +8,14 @@ async fn main() {
     // Read SV
     let pv1 = inst.read_registers(0x1001, 1).await.unwrap();
     println!("pv is now {:?}", pv1);
-    
+
     // Set SV
     inst.write_register(0x1001, 1300).await.unwrap();
-    
+
     // Read SV again
     let pv2 = inst.read_registers(0x1001, 1).await.unwrap();
     println!("pv is now {:?}", pv2);
-    
+
     // Set PID to STOP
     inst.write_coil(0x0814, false).await.unwrap();
     // Read run/stop value
