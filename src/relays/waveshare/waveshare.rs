8cf0456d96a58bd2608aa36a202eea3d596474dc
llamicron <llamicron@gmail.com>
2021-06-15 10:14:43 -0500

    Adds doc comments to the Waveshare module

--- src/relays/waveshare/waveshare.rs
+++ src/relays/waveshare/waveshare.rs
@@ -1,34 +1,75 @@
+// Used for checksums
 use crc::{Crc, CRC_16_MODBUS};
+
+// generic board stuff
 use crate::relays::{Board, BoardError, State};
 
 type Result<T> = std::result::Result<T, BoardError>;
 
+// This is the checksum algorithm that the board uses
 const CRC_MODBUS: Crc<u16> = Crc::<u16>::new(&CRC_16_MODBUS);
+// Hardcode the baud, we *probably* won't need to change it
 const WAVESHARE_BAUD: usize = 9600;
 
+/// A Waveshare board.
+/// 
+/// These boards are relatively cheap and (so far) reliable. They can be [found here](https://www.waveshare.com/modbus-rtu-relay.htm).
+/// The [operation wiki](https://www.waveshare.com/wiki/Protocol_Manual_of_Modbus_RTU_Relay) explains how to use it, but you probably won't need that.
+/// 
+/// See the `examples/` directory for a complete example of using this board. Here's a sneak peak
+/// ```rust
+/// use brewdrivers::relays::{Waveshare, State};
+/// 
+/// let mut ws = Waveshare::connect(0x01, "/dev/ttyUSB0").unwrap();
+/// ws.set_relay(3, State::On).unwrap(); // Turn on the 4th relay (indexed from 0)
+/// println!("{:?}", ws.get_all_relays().unwrap());
+/// ```
 #[derive(Debug)]
 pub struct Waveshare(Board);
 
 impl Waveshare {
+    /// Connect to a board at the given address and port. This will fail if the port can't be opened,
+    /// or if the board can't be communicated with. This method will poll the board for it's software 
+    /// version number and fail if it doesn't return one, returning a [`BoardError`](crate::relays::BoardError).
+    /// 
+    /// ```rust
+    /// use brewdrivers::relays::Waveshare;
+    /// 
+    /// let mut ws = Waveshare::connect(0x01, "/dev/ttyUSB0").unwrap();
+    /// ws.get_relay(0).unwrap();
+    /// // ...
+    /// ```
     pub fn connect(address: u8, port_path: &str) -> Result<Waveshare> {
         let port = Board::open_port(port_path, WAVESHARE_BAUD).map_err(|err| BoardError(format!("{}", err)) );
+        let mut ws = Waveshare(Board {
+            address,
+            port: port?,
+            baudrate: WAVESHARE_BAUD
+        });
 
-        Ok(
-            Waveshare(Board {
-                address,
-                port: port?,
-                baudrate: WAVESHARE_BAUD
-            })
-        )
+        ws.software_revision()?;
+
+        Ok(ws)
     }
 
-    pub fn append_checksum(bytes: &mut Vec<u8>) -> Result<()> {
+    // Calculates the CRC checksum for the data bytes to send to the board
+    fn append_checksum(bytes: &mut Vec<u8>) -> Result<()> {
         let checksum = CRC_MODBUS.checksum(&bytes).to_le_bytes();
         bytes.push(checksum[0]);
         bytes.push(checksum[1]);
         Ok(())
     }
 
+    /// Sets a relay to the given state. See the [`State`](crate::relays::State) enum.
+    /// 
+    /// ```rust
+    /// use brewdrivers::relays::Waveshare;
+    /// 
+    /// let mut ws = Waveshare::connect(0x01, "/dev/ttyUSB0").unwrap();
+    /// ws.set_relay(0, State::On).unwrap();
+    /// assert_eq!(ws.get_relay(0).unwrap(), State::On);
+    /// ws.set_relay(0, State::Off);
+    /// ```
     pub fn set_relay(&mut self, relay_num: u8, state: State) -> Result<()> {
         // Example: 01 05 00 00 FF 00 8C 3A
         // 01   	Device address	    0x00 is broadcast address；0x01-0xFF are device addresses
@@ -62,6 +103,7 @@ impl Waveshare {
         Ok(())
     }
 
+    /// Gets a relay state. See [`State`](crate::relays::State).
     pub fn get_relay(&mut self, relay_num: u8) -> Result<State> {
         let statuses: Vec<State> = self.get_all_relays()?;
         
@@ -78,6 +120,7 @@ impl Waveshare {
         }
     }
 
+    /// Sets all relays at once to the given state.
     pub fn set_all_relays(&mut self, state: State) -> Result<()> {
         let mut bytes: Vec<u8> = vec![
             self.0.address(),
@@ -103,6 +146,7 @@ impl Waveshare {
         Ok(())
     }
 
+    /// Returns a `Vec<State>` of all 8 relays.
     pub fn get_all_relays(&mut self) -> Result<Vec<State>> {
         let mut bytes: Vec<u8> = vec![
             self.0.address(),
@@ -129,6 +173,36 @@ impl Waveshare {
                 BoardError(format!("Board did not return the proper response, got {:?}", resp))
             )
         }
+    }
+
+    /// Returns the software revision as a String like "v1.00"
+    /// 
+    /// ```rust
+    /// use brewdrivers::relays::Waveshare;
+    /// 
+    /// let mut ws = Waveshare::connect(0x01, "/dev/ttyUSB0").unwrap()
+    /// assert_eq!(ws.software_revision().unwrap(), "v1.00");
+    /// ```
+    pub fn software_revision(&mut self) -> Result<String> {
+        let mut bytes: Vec<u8> = vec![
+            self.0.address(),
+            0x03,
+            0x20, 0x00,
+            0x00, self.0.address(),
+        ];
+
+        Waveshare::append_checksum(&mut bytes)?;
+
+        let resp = self.0.write_to_device(bytes)?;
+
+        if let Some(&version_num) = resp.get(3) {
+            Ok(format!("v{:.2}", (version_num as f64 / 100.0)))
+        } else {
+            Err(BoardError(
+                format!("The board didn't return it's software revision correctly. Response: {:?}. Possible connection issue.", resp)
+            ))
+        }
+
     }
 }
 
@@ -226,4 +300,12 @@ mod tests {
 
 
     }
+
+    #[test]
+    #[serial]
+    fn test_software_revision() {
+        let mut ws = ws();
+        assert_eq!(ws.software_revision().unwrap(), "v1.00");
+    }
+
 }
