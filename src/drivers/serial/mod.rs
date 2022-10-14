49aa68670c982bb8e522183db6162abebaed94bb
llamicron <llamicron@gmail.com>
2022-10-13 22:48:49 -0500

    Removes unused BoardError

--- src/drivers/serial/mod.rs
+++ src/drivers/serial/mod.rs
@@ -1,8 +1,6 @@
 pub mod bytestring;
 pub mod instrument;
-pub mod error;
 
 pub use bytestring::Bytestring;
 pub use instrument::SerialInstrument;
 pub use instrument::State;
-pub use error::BoardError;
